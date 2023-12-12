c34d0042c1c166a9f5ff12e62d3df51ce02f9fc3
Sean M <143843703+iamseaniam@users.noreply.github.com>
2023-12-12 16:38:40 -0600

    Update instruction_t.c

--- instruction_t.c
+++ instruction_t.c
@@ -1,5 +1,8 @@
 #include "monty.h"
 
+void pall_opcode(stack_t **stack, unsigned int line_number);
+void push_opcode(stack_t **stack, unsigned int line_number);
+
 instruction_t instructions[] = {
 	{"push", push_opcode},
 	{"pall", pall_opcode},
